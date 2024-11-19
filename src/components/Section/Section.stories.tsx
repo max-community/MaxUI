f35a7b6fb3af941b9615c4359d0119b5b1d00470
vadim.tvj <vadim.tvj@gmail.com>
2024-11-19 19:02:14 +0300

    refactor(Section): исправлены некорректные пропы в сторибуках

--- src/components/Section/Section.stories.tsx
+++ src/components/Section/Section.stories.tsx
@@ -15,7 +15,7 @@ const meta = {
   decorators: [
     (Story) => (
       <div style={{ width: 375, height: 375 }}>
-        <Panel mode="secondary" centered>
+        <Panel mode="secondary" centeredY>
           <Story />
         </Panel>
       </div>
@@ -31,7 +31,7 @@ export const Playground: Story = {
     return (
       <Section {...args}>
         <Cell
-          heading="Igor Foxik"
+          title="Igor Foxik"
           before={(
             <Avatar.Container size={40}>
               <Avatar.Image src="https://sun9-67.userapi.com/s/v1/ig2/JsDOGxKlelRtyPp3DgWYYxPbSMfsENZjZdWiDANWbV7MEmsJjdIivjgHhWTH5JlNJCJVX-KBKhIYahjkbjzvxxhD.jpg?quality=95&crop=344,13,854,854&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720&ava=1&cs=100x100" />
@@ -41,7 +41,7 @@ export const Playground: Story = {
           onClick={() => {}}
         />
         <Cell
-          heading="Vadim Tregubenko"
+          title="Vadim Tregubenko"
           before={(
             <Avatar.Container size={40}>
               <Avatar.Image src="https://sun9-67.userapi.com/s/v1/ig2/CY_xDesKnMtl0OiJynK0oc7QnxQVJUgeciJSi_MpZUiE3EHSCNltr76jugXaygGd2Xh0M8-61v7Jwfl1kO87YWVe.jpg?quality=95&crop=0,0,1440,1440&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,1080x1080,1280x1280,1440x1440&ava=1&u=SpmuDKJYdLKKRYYDgjLVQdEn6QnBonR3kSYxCSkCnm4&cs=200x200" />
@@ -51,7 +51,7 @@ export const Playground: Story = {
           onClick={() => {}}
         />
         <Cell
-          heading="Ira Grishina"
+          title="Ira Grishina"
           before={(
             <Avatar.Container size={40}>
               <Avatar.Image src="https://sun9-22.userapi.com/s/v1/ig2/GIx9ixLIVcH_Cu_E5hwjYXqHSDGEO6XkE33wfKgVvs1c33VaMO24otPJ_nrgWHt7dOP1B3kWEYxoIezkFxu9ygxZ.jpg?quality=95&crop=100,579,1010,1010&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720&ava=1&cs=100x100" />
@@ -61,7 +61,7 @@ export const Playground: Story = {
           onClick={() => {}}
         />
         <Cell
-          heading="Misha Zubkov"
+          title="Misha Zubkov"
           before={(
             <Avatar.Container size={40}>
               <Avatar.Image src="https://sun9-47.userapi.com/s/v1/ig2/-DYTYLapJ8r0r9wLZfAYI1OrgscIeYSwQd4o-xSzEmp0hFisghOOWRYkEwn-ZHguTPnTVvb5weq3kwAhlgPqAfWi.jpg?quality=95&crop=0,145,1800,1800&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,1080x1080,1280x1280,1440x1440&ava=1&cs=100x100" />
